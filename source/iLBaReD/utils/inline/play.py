078667fc293b9b50ed47aabcc67f5b223097ac54
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-30 06:41:32 +0300

    Add files via upload

---
+++ source/iLBaReD/utils/inline/play.py
@@ -0,0 +1,128 @@
+import math
+from iLBaReD import app 
+from pyrogram.types import InlineKeyboardButton
+from iLBaReD.utils.formatters import time_to_seconds
+
+def track_markup(_, videoid, user_id, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
+    ]
+    return buttons
+
+
+def stream_markup_timer(_, chat_id, played, dur):
+    [
+            InlineKeyboardButton(text="⦗ استأناف ▷ ⦘", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⦗ ايقاف مؤقت II ⦘", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⦗ إعادة تشغيل ↻ ⦘", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="⦗ تخطي ‣‣I ⦘", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⦗ ايقاف ▢ ⦘", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="• ⌯ 𝐒𝐎𝐔𝐑𝐂𝐄.𝐀𝐃𝐑𝐄𝐍𝐀𝐋𝐄𝐍 ⌯ •", url=f"https://t.me/WA_AdRenalen"),
+        ],[
+            InlineKeyboardButton(text="اضف البوت الي جروبك او قناتك 🎸 ⋅", url=f"https://t.me/{app.username}?startgroup=true")]
+    return buttons
+
+
+def stream_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(text="⦗ استأناف ▷ ⦘", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⦗ ايقاف مؤقت II ⦘", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⦗ إعادة تشغيل ↻ ⦘", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="⦗ تخطي ‣‣I ⦘", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⦗ ايقاف ▢ ⦘", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="• ⌯ 𝐒𝐎𝐔𝐑𝐂𝐄.𝐀𝐃𝐑𝐄𝐍𝐀𝐋𝐄𝐍 ⌯ •", url=f"https://t.me/WA_AdRenalen"),
+        ],[
+            InlineKeyboardButton(text="اضف البوت الي جروبك او قناتك 🎸 ⋅", url=f"https://t.me/{app.username}?startgroup=true")],
+    ]
+    return buttons
+
+
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def livestream_markup(_, videoid, user_id, mode, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_3"],
+                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
+    query = f"{query[:20]}"
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="◁",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {query}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="Resume",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+        ],
+    ]
+    return buttons
