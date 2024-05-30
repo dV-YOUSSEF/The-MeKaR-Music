635e29c88497ee3c86f86aedfd54c35c0d9cf68f
The-Old-Fazar <166647185+The-Old-Fazar@users.noreply.github.com>
2024-05-30 09:27:43 +0300

    Update maker.py

--- Maker/maker.py
+++ Maker/maker.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, Client
 from pyrogram import Client as app
-from config import API_ID, API_HASH, MONGO_DB_URL, appp, user as usr, helper as ass, call, OWNER, OWNER_NAME, CHANNEL, GROUP, VIDEO
+from config import API_ID, API_HASH, MONGO_DB_URL, appp, user as usr, helper as ass, call, OWNER, OWNER_NAME, CHANNEL, GROUP
 from source.info import Call, activecall, helper, active
 from source.Data import db, dev, devname, set_must, get_data
 from pyrogram.raw.types import InputPeerChannel
